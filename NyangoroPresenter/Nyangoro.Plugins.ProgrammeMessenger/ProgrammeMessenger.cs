ccb19a6be1089b0cdd011151611e05684641ad03
Jakub Stribrny <stribrny@1webit.cz>
2014-07-13 15:18:10 +0200

    Started work on ProgrammeMessenger plugin

---
+++ NyangoroPresenter/Nyangoro.Plugins.ProgrammeMessenger/ProgrammeMessenger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nyangoro.Plugins;
+using Nyangoro.Interfaces;
+
+namespace Nyangoro.Plugins.ProgrammeMessenger
+{
+    /**
+     * This plugin displays programme information and custom messages
+     * on the bottom of the presentation screen.
+     * 
+     * @TODO: split into two plugins that will be switched between - ProgrammeDisplay
+     * and CustomMessagesDisplay
+     */
+    public class ProgrammeMessenger : Nyangoro.Plugins.Plugin
+    {
+        new public ProgrammeMessengerController Controller { get { return (ProgrammeMessengerController)this.controller; } set { this.controller = value; } }
+
+        public Nyangoro.Plugins.ProgrammeMessenger.ControlRoot ControlRoot { get { return (ControlRoot)this.controlRoot; } set { this.controlRoot = value; } }
+    }
+}
